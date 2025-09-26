99347b3a1934d259b8d2cab6a5fc4e58cdf4f10e
henry <henry@stabro.ai>
2025-09-26 11:27:51 +0800

    update, fix

--- scripts/cp-lets-crpyto-cert.sh
+++ scripts/cp-lets-crpyto-cert.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd config-generated
+rm certs/*.*
 cp certs-lets/certificate.crt certs/nurokmail.com.pem
 cp certs-lets/fullchain.crt certs/fullchain.pem
 cp certs-lets/chain.crt certs/chain.pem
