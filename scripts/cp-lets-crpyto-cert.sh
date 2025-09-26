820bce6512d3bed736a382b799f82ded051d6876
henry <henry@stabro.ai>
2025-09-26 11:26:49 +0800

    update, fix

---
+++ scripts/cp-lets-crpyto-cert.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd config-generated
+cp certs-lets/certificate.crt certs/nurokmail.com.pem
+cp certs-lets/fullchain.crt certs/fullchain.pem
+cp certs-lets/chain.crt certs/chain.pem
+cp certs-lets/nurokmail.com-key.pem certs/nurokmail.com-key.pem
+cd ..
